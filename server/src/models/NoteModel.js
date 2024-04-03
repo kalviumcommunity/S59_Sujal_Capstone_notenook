15bcd16bc7cd9dbda7cb1e58dee17871b54e4202
Sujal Shresth <sujshrest@example.com>
2024-04-03 17:08:36 +0530

    Mergeded the Markdown Schema with Note Schema

--- server/src/models/NoteModel.js
+++ server/src/models/NoteModel.js
@@ -1,13 +1,23 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const markdownSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+
+  title: { type: String, required: true },
+
+  content: { type: String, required: true },
+});
+
 const noteSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
 
   subject: { type: String, required: true },
 
   fileReference: { type: String, required: true },
+
+  markDownNote: markdownSchema,
 });
 
-const NoteModel = new mongoose.model("Note", noteSchema);
+const NoteModel = mongoose.model("Note", noteSchema);
 module.exports = { NoteModel };
