4b78658d9cf961e1bfe6e890f28eef036920dc61
Sujal Shresth <sujshrest@example.com>
2024-04-17 15:42:56 +0530

    Initialized the AddNotes Page

--- client/src/components/TextEditor.jsx
+++ client/src/components/TextEditor.jsx
@@ -1,8 +1,51 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Quill from "quill";
+import "quill/dist/quill.snow.css";
 
 function TextEditor() {
-  return <div></div>;
+  const wrapperRef = useCallback((wrapper) => {
+    console.log(wrapper);
+    if (wrapper == null) return;
+    wrapper.innerHTML = "";
+    const editor = document.createElement("div");
+
+    wrapper.append(editor);
+    const toolbarOptions = [
+      ["bold", "italic", "underline", "strike"],
+      ["blockquote", "code-block"],
+      ["link"],
+
+      [{ header: 1 }, { header: 2 }],
+      [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
+      [{ script: "sub" }, { script: "super" }],
+      [{ indent: "-1" }, { indent: "+1" }],
+      [{ direction: "rtl" }],
+
+      [{ size: ["small", false, "large", "huge"] }],
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+      [{ color: [] }, { background: [] }],
+      [{ font: [] }],
+      [{ align: [] }],
+
+      ["clean"],
+    ];
+    new Quill(editor, {
+      modules: {
+        toolbar: toolbarOptions,
+      },
+      placeholder: "Compose an epic...",
+      theme: "snow",
+    });
+  }, []);
+
+  return (
+    <>
+      <div id="container" ref={wrapperRef}>
+        Text Editor lorem100
+      </div>
+    </>
+  );
 }
 
 export default TextEditor;
---
+++ client/src/pages/AddNotes.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import TextEditor from "../components/TextEditor";
+import "../css/AddNotes.css";
+
+function AddNotes() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="addNotesPage">
+      <TextEditor />
+      <div>
+        <form className="noteDetails" onSubmit={handleSubmit(onSubmit)}>
+          <div className="field">
+            <label htmlFor="noteTitle">Note Title:</label>
+            <input
+              type="text"
+              name="noteTitle"
+              id="noteTitle"
+              {...register("noteTitle", { required: "Note title is required" })}
+            />
+
+            <p className="error">{errors.noteTitle?.message}</p>
+          </div>
+
+          <div className="field">
+            <label htmlFor="subject">Subject:</label>
+            <input
+              type="text"
+              name="subject"
+              id="subject"
+              {...register("subject", { required: "Subject is required" })}
+            />
+
+            <p className="error">{errors.subject?.message}</p>
+          </div>
+
+          <div className="field">
+            <label htmlFor="chapter">Chapter:</label>
+            <input
+              type="text"
+              name="chapter"
+              id="chapter"
+              {...register("chapter", { required: "Chapter is required" })}
+            />
+
+            <p className="error">{errors.chapter?.message}</p>
+          </div>
+
+          <div className="file-input-container">
+            <input type="file" id="fileInput" className="file-input" />
+            <label htmlFor="fileInput" className="file-input-label">
+              Upload files
+            </label>
+          </div>
+
+          <button type="submit" className="button">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default AddNotes;
