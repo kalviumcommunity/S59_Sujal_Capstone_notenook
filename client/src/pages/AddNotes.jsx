291d030352658c2ffcddde2b43ae4387f6f46276
Sujal Shresth <sujshrest@example.com>
2024-04-19 13:16:05 +0530

    Initialized myNotes page

--- client/src/pages/AddNotes.jsx
+++ client/src/pages/AddNotes.jsx
@@ -1,15 +1,42 @@
-import React, { useState } from "react";
+import React from "react";
 import TextEditor from "../components/TextEditor";
 import "../css/AddNotes.css";
+import { Routes, Route, Link } from "react-router-dom";
 import NoteDetailsForm from "../components/NoteDetailsForm";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 function AddNotes() {
   return (
     <div className="addNotesPage">
       <div className="addNotes">
-        <TextEditor />
-        <NoteDetailsForm></NoteDetailsForm>
+        <Routes>
+          <Route path="/" element={<MyNotesList />}></Route>
+          <Route path="/newNote" element={<NewNote />}></Route>
+        </Routes>
       </div>
     </div>
+  );
+}
+
+function MyNotesList() {
+  return (
+    <>
+      <div className="myNotesList">
+        <div className="flex  w-full justify-between">
+          <h1 className="heading">My Notes</h1>
+          <Link to={"newNote"}>
+            <button className="button">New Note</button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
+function NewNote() {
+  return (
+    <>
+      <TextEditor />
+      <NoteDetailsForm />
+    </>
   );
 }
 
