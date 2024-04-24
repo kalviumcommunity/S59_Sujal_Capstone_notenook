f104723b407395a3518a47df4f56306accbfd7eb
Sujal Shresth <sujshrest@example.com>
2024-04-24 15:02:06 +0530

    Defined the socket for text-editor

--- client/src/components/TextEditor.jsx
+++ client/src/components/TextEditor.jsx
@@ -1,9 +1,66 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 import ToolBar from "./ToolBar";
 import "../css/TextEditor.css";
+import { io } from "socket.io-client";
+import { useParams } from "react-router-dom";
+
 function TextEditor() {
+  const { documentId } = useParams();
+  const [socket, setSocket] = useState();
+  const [quill, setQuill] = useState();
+
+  useEffect(() => {
+    const socketConnection = io(
+      import.meta.env.VITE_REACT_APP_TEXT_EDITOR_SOCKET
+    );
+
+    setSocket(socketConnection);
+    return () => {
+      socketConnection.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (socket == null || quill == null) {
+      return;
+    }
+    const handler = (delta, oldDelta, source) => {
+      if (source !== "user") {
+        return;
+      }
+      socket.emit("send-changes", delta);
+    };
+
+    quill.on("text-change", handler);
+
+    return () => {
+      quill.off("text-change", handler);
+    };
+  }, [socket, quill]);
+
+  useEffect(() => {
+    if (socket == null || quill == null) {
+      return;
+    }
+
+    socket.emit("get-document", documentId);
+
+    socket.once("load-document", (document) => {
+      quill.setContents(document);
+      quill.enable();
+    });
+
+    const interval = setInterval(() => {
+      socket.emit("save-document", quill.getContents());
+    }, 2000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [socket, quill, documentId]);
+
   const wrapperRef = useCallback((wrapper) => {
     console.log(wrapper);
     if (wrapper == null) return;
@@ -11,12 +68,15 @@ function TextEditor() {
     const editor = document.createElement("div");
 
     wrapper.append(editor);
-    new Quill(editor, {
+    const q = new Quill(editor, {
       modules: {
         toolbar: { container: "#toolbar" },
       },
       theme: "snow",
     });
+    q.disable();
+    q.setText("Loading document......");
+    setQuill(q);
   }, []);
 
   return (
--- client/src/pages/AddNotes.jsx
+++ client/src/pages/AddNotes.jsx
@@ -1,62 +1,18 @@
-import React, { useContext, useState } from "react";
-import TextEditor from "../components/TextEditor";
+import React from "react";
 import "../css/AddNotes.css";
-import { Routes, Route, Link } from "react-router-dom";
-import NoteDetailsForm from "../components/NoteDetailsForm";
-import MyNoteList from "../components/MyNoteList";
-import NewNoteForm from "../components/NewNoteForm";
-import { DeviceWidthContext } from "../context/deviceWidthContext";
+import { Routes, Route } from "react-router-dom";
+import WriteNote from "../components/WriteNote";
+import MyNotes from "../components/MyNotes";
 function AddNotes() {
   return (
     <div className="addNotesPage relative">
       <div className="addNotes">
         <Routes>
           <Route path="/" element={<MyNotes />}></Route>
-          <Route path="/newNote" element={<NewNote />}></Route>
+          <Route path="/writeNote/:documentId" element={<WriteNote />}></Route>
         </Routes>
       </div>
     </div>
-  );
-}
-
-function MyNotes() {
-  const [postNewNote, setPostNewNote] = useState(false);
-  const width = useContext(DeviceWidthContext);
-  const handleClick = () => {
-    setPostNewNote(!postNewNote);
-  };
-  return (
-    <>
-      {postNewNote && (
-        <NewNoteForm
-          postNewNote={postNewNote}
-          setPostNewNote={setPostNewNote}
-          handleClick={handleClick}
-        />
-      )}
-      <MyNoteList
-        postNewNote={postNewNote}
-        setPostNewNote={setPostNewNote}
-        handleClick={handleClick}
-      />
-
-      {width < 1024 && (
-        <div className="w-full flex justify-between items-center flex-col">
-          <p className="mb-4">Wanna add a note?</p>
-          <Link to={"newNote"}>
-            <button className="button">Upload a new note?</button>
-          </Link>
-        </div>
-      )}
-    </>
-  );
-}
-function NewNote() {
-  return (
-    <>
-      <TextEditor />
-      <NoteDetailsForm />
-    </>
   );
 }
 
