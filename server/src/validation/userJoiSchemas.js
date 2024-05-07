b719e2451ca81a78d6a58093da4d3fa20f54be75
Sujal Shresth <sujshrest@example.com>
2024-05-07 14:20:02 +0530

    Added User Data Update Put endpoint

--- server/src/validation/userJoiSchemas.js
+++ server/src/validation/userJoiSchemas.js
@@ -1,17 +1,17 @@
 const Joi = require("joi");
 
-const userUpdateJoiSchema = Joi.object({
-  username: Joi.string().min(3).optional(),
-  fullname: Joi.string().required(),
-  password: Joi.string().required(),
-  newPassword: Joi.string().min(6).optional(),
-});
-
 const userJoiSchema = Joi.object({
   username: Joi.string().min(3).required(),
   fullname: Joi.string().required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
+});
+
+const userUpdateJoiSchema = Joi.object({
+  username: Joi.string().min(3).optional(),
+  fullname: Joi.string().required(),
+  password: Joi.string().required(),
+  newPassword: Joi.string().min(6).optional(),
 });
 
 module.exports = {
