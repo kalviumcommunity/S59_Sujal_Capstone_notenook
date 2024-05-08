98306f6157e818909a90672f88a02f36e14ed465
Sujal Shresth <sujshrest@example.com>
2024-05-08 16:56:54 +0530

    Created requests for get user info, and update info

--- client/src/components/UserProfileComponents/UpdatePasswordForm.jsx
+++ client/src/components/UserProfileComponents/UpdatePasswordForm.jsx
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+import extractTokenFromCookie from "../../Functions/ExtractTokenFromCookie";
 
 function UpdatePasswordForm() {
   const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
   const {
     register,
     formState: { errors },
@@ -10,7 +15,37 @@ function UpdatePasswordForm() {
     watch,
   } = useForm();
 
-  const submitForm = async (data) => {};
+  const submitForm = async (data) => {
+    try {
+      const token = extractTokenFromCookie();
+      if (!token) {
+        throw new Error("Token not found");
+      }
+
+      const response = await axios.patch(
+        import.meta.env.VITE_REACT_APP_USER_DETAIL_UPDATE_PASSOWRD_ENDPOINT,
+        data,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        alert("Updated password successflully");
+        navigate("/notenook/profile");
+      } else {
+        setErrorMessage("Something went wrong. Please try again later.");
+      }
+    } catch (error) {
+      if (error.response && error.response.data) {
+        setErrorMessage(error.response.data.message);
+      } else {
+        setErrorMessage("Something went wrong. Please try again later.");
+      }
+    }
+  };
 
   return (
     <form onSubmit={handleSubmit(submitForm)} className="updatePasswordForm">
@@ -24,14 +59,6 @@ function UpdatePasswordForm() {
           name="password"
           {...register("password", {
             required: "Enter Password",
-            minLength: {
-              value: 10,
-              message: "Password Must be longer than 10 characters",
-            },
-            pattern: {
-              value: /^(?=.*[!@#$%^&*])/,
-              message: "Password must contain at least one special character",
-            },
           })}
         />
         <p className="error">{errors.password?.message || errorMessage}</p>
--- client/src/pages/NoteNookPages/UserProfile.jsx
+++ client/src/pages/NoteNookPages/UserProfile.jsx
@@ -1,23 +1,61 @@
-import "../../css/UserProfile.css";
+import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import axios from "axios";
+
 import UserInfo from "../../components/UserProfileComponents/UserInfo";
 import UpdatePasswordForm from "../../components/UserProfileComponents/UpdatePasswordForm";
 import UpdateUserForm from "../../components/UserProfileComponents/UpdateUserInfoForm";
-import { Link, Route, Routes } from "react-router-dom";
+import extractTokenFromCookie from "../../Functions/ExtractTokenFromCookie";
+
+import "../../css/UserProfile.css";
+
 function UserProfile() {
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      const token = extractTokenFromCookie();
+      if (!token) {
+        throw new Error("Token not found");
+      }
+      try {
+        const response = await axios.get(
+          import.meta.env.VITE_REACT_APP_USER_DETAIL_ENDPOINT,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        setUserInfo(response.data.user);
+      } catch (error) {
+        console.error("Error fetching user details:", error);
+      }
+    };
+
+    fetchUserDetails();
+  }, []);
+
   return (
     <div className="userProfilePage">
-      <UserInfo />
+      <UserInfo userInfo={userInfo} />
       <Routes>
-        <Route path="edit" element={<UpdateForms />}></Route>
+        <Route
+          path="edit"
+          element={
+            <UpdateForms userInfo={userInfo} setUserInfo={setUserInfo} />
+          }
+        ></Route>
       </Routes>
     </div>
   );
 }
 
-function UpdateForms() {
+function UpdateForms({ userInfo, setUserInfo }) {
   return (
     <div className="updateForms">
-      <UpdateUserForm />
+      <UpdateUserForm userInfo={userInfo} setUserInfo={setUserInfo} />
       <UpdatePasswordForm />
     </div>
   );
