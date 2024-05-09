af2aebc29d39df4cdd66fd5a19031232400a1aa0
Sujal Shresth <sujshrest@example.com>
2024-05-09 12:51:54 +0530

    Created confirmation for note create and delete

--- client/src/components/AddNotesComponent/MyNote.jsx
+++ client/src/components/AddNotesComponent/MyNote.jsx
@@ -1,10 +1,31 @@
 import { Link } from "react-router-dom";
-
+import { useState } from "react";
 import formatDate from "../../Functions/FormatDate";
 
-function MyNote({ note }) {
+function MyNote({ note, handleDelete }) {
+  const [deleteConfirmation, setDeleteConfirmation] = useState(false);
+
   return (
-    <div className="myNote">
+    <div className="myNote relative">
+      {deleteConfirmation && (
+        <div className="confirmation-popup">
+          <p className="heading">Are you sure you want to delete this note?</p>
+          <div className="flex gap-10">
+            <button
+              onClick={() => handleDelete(note._id)}
+              className="button yes-button"
+            >
+              Yes
+            </button>
+            <button
+              onClick={() => setDeleteConfirmation(false)}
+              className="button cancel-button"
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      )}
       <Link to={`/notenook/viewNote/${note._id}`}>
         <div className="noteInfo">
           <p className="title">
@@ -21,7 +42,12 @@ function MyNote({ note }) {
         <Link to={`writeNote/${note._id}`}>
           <button className="update button">Update</button>
         </Link>
-        <button className="delete button">Delete</button>
+        <button
+          onClick={() => setDeleteConfirmation(true)}
+          className="delete button"
+        >
+          Delete
+        </button>
       </div>
     </div>
   );
