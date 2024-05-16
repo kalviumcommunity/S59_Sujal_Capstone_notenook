0e9fca1b67c132369b7afb0ce02aa83d7dcdb03a
Sujal Shresth <sujshrest@example.com>
2024-05-16 18:25:36 +0530

    Added Googhle OAuth using Passport and express-session

--- server/server.js
+++ server/server.js
@@ -2,12 +2,15 @@
 require("dotenv").config();
 require("./src/auth/LocalStrategy");
 require("./src/auth/JwtStrategy");
+require("./src/auth/GoogleOAuth");
 
 // importing packages
 const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
 const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -26,8 +29,25 @@ app.use(
   cors({
     origin: process.env.CLIENT_URI,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
   })
 );
+
+// Initialize express-session middleware
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secret: process.env.SESSION_SECRET,
+    },
+  })
+);
+
+// Initialize Passport and restore authentication state\
+app.use(passport.initialize());
+app.use(passport.session());
 
 // creating a socket instance
 const io = socketIO(server, {
@@ -45,11 +65,13 @@ textEditorSocket(textEditorNamespace);
 //  setting up routes
 const userRouter = require("./src/routes/userRoutes");
 const noteRouter = require("./src/routes/noteRoutes");
+const googleAuthRouter = require("./src/routes/googleOAuthRoutes");
 const verificationRouter = require("./src/routes/verificationRoutes");
 
 app.use("/user", userRouter);
 app.use("/note", noteRouter);
 app.use("/verification", verificationRouter);
+app.use("/auth", googleAuthRouter);
 
 // setting up the server to listen
 app.get("/", (req, res) => {
