c58cdd25a61e88d85fb7d6b2c74d2b2c0d8d7728
Sujal Shresth <sujshrest@example.com>
2024-06-15 13:12:31 +0545

    Added authenticate socket handshake middleware

--- server/src/socketHandlers/chatSocket.js
+++ server/src/socketHandlers/chatSocket.js
@@ -1,4 +1,6 @@
-const jwt = require("jsonwebtoken");
+const {
+  authenticateSocketHandshake,
+} = require("../middlewares/socketHandshakeAuthentication");
 const { redisClient } = require("../config/redisConfig");
 
 let chatNamespace = null;
@@ -6,32 +8,7 @@ let chatNamespace = null;
 function chatSocket(ioNamespace) {
   chatNamespace = ioNamespace;
 
-  chatNamespace.use((socket, next) => {
-    const token = socket.handshake.auth.token;
-    if (!token) {
-      return next(new Error("Authentication error: Token not provided"));
-    }
-
-    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
-      if (err) {
-        let errorMessage = "Authentication error";
-        if (err.name === "TokenExpiredError") {
-          errorMessage = "Authentication error: Token has expired";
-        } else if (err.name === "JsonWebTokenError") {
-          errorMessage = "Authentication error: Invalid token";
-        }
-        return next(new Error(errorMessage));
-      }
-
-      if (!decoded.userId) {
-        return next(new Error("Authentication error: Invalid token payload"));
-      }
-
-      socket.userId = decoded.userId;
-      socket.username = decoded.username;
-      next();
-    });
-  });
+  chatNamespace.use(authenticateSocketHandshake);
 
   chatNamespace.on("connection", async (socket) => {
     await setSocketUserId(socket.userId, socket.id);
