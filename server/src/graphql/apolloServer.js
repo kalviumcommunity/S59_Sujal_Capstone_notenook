7a611c1d8205f52ede61e1206bd95c50e147c4da
Sujal Shresth <sujshrest@example.com>
2024-06-24 18:26:58 +0545

    Added Comments model and defined schema and resolvers to fetch comments using graphQL

---
+++ server/src/graphql/apolloServer.js
@@ -0,0 +1,9 @@
+const { ApolloServer } = require("@apollo/server");
+const { typeDefs, resolvers } = require("./schema");
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+module.exports = apolloServer;
---
+++ server/src/graphql/schema.js
@@ -0,0 +1,47 @@
+const { NoteModel } = require("../models/NoteModel");
+const { CommentModel } = require("../models/CommentModel");
+const typeDefs = `#graphql
+  type User {
+    _id: ID!
+    username: String!
+  }
+
+  type Comment {
+    _id: ID!
+    postedBy: User!
+    comment: String!
+    createdAt: String!
+    updatedAt: String!
+  }
+
+  type Query {
+    getCommentsByNoteId(noteId: ID!): [Comment]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getCommentsByNoteId: async (_, { noteId }) => {
+      try {
+        const note = await NoteModel.findById(noteId).populate({
+          path: "comments",
+          populate: {
+            path: "postedBy",
+            select: "_id username", 
+          },
+        });
+
+        if (!note) {
+          throw new Error("Note not found");
+        }
+
+        return note.comments;
+      } catch (error) {
+        console.error("Error fetching comments:", error);
+        throw new Error("Failed to fetch comments");
+      }
+    },
+  },
+};
+
+module.exports = { typeDefs, resolvers };
